4e114ae0a746570ee44fbc3e5418326f5569a7f6
artp <dky7034@naver.com>
2025-05-07 18:48:17 +0900

    feat: #33 토스 개별 결제 구현완료, 장바구니 결제 구현 중

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
       <div className="footer-notice-bar">
         <div className="notice-left">
           <strong>공지사항</strong>
-          <span>기프트카드 이용약관 변경 안내 (3/28)</span>
+          <span>어버이날 이벤트 안내 (5/8)</span>
         </div>
         <div className="notice-right">
           <strong>당첨자발표</strong>
@@ -26,14 +26,32 @@ function Footer() {
 
         {/* 중앙: 링크 */}
         <ul className="footer-link-list">
-          <li><a href="#">회사소개</a></li>
-          <li><a href="#">이용약관</a></li>
-          <li><a href="#"><strong>개인정보처리방침</strong></a></li>
-          <li><a href="#">청소년보호정책</a></li>
-          <li><a href="#">대량주문안내</a></li>
-          <li><a href="#">협력사</a></li>
-          <li><a href="#">채용정보</a></li>
-          <li><a href="#">광고소개</a></li>
+          <li>
+            <a href="#">회사소개</a>
+          </li>
+          <li>
+            <a href="#">이용약관</a>
+          </li>
+          <li>
+            <a href="#">
+              <strong>개인정보처리방침</strong>
+            </a>
+          </li>
+          <li>
+            <a href="#">청소년보호정책</a>
+          </li>
+          <li>
+            <a href="#">대량주문안내</a>
+          </li>
+          <li>
+            <a href="#">협력사</a>
+          </li>
+          <li>
+            <a href="#">채용정보</a>
+          </li>
+          <li>
+            <a href="#">광고소개</a>
+          </li>
         </ul>
 
         {/* 오른쪽: 소셜/패밀리사이트 */}
@@ -44,17 +62,29 @@ function Footer() {
             <option>핫트랙스</option>
           </select>
           <div className="footer-social-icons">
-            <a href="#"><i className="fab fa-facebook-f"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
-            <a href="#"><i className="fab fa-youtube"></i></a>
+            <a href="#">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-youtube"></i>
+            </a>
           </div>
         </div>
       </div>
 
       {/* 하단 정보 */}
       <div className="footer-bottom-info">
-        <p>대표이사 : 허정도 | 서울특별시 종로구 종로1 | 사업자등록번호 : 102-81-11670</p>
-        <p>대표전화 : 1544-1900 | FAX : 0502-987-5711 | 통신판매업신고번호 : 제653호</p>
+        <p>
+          대표이사 : 허정도 | 서울특별시 종로구 종로1 | 사업자등록번호 :
+          102-81-11670
+        </p>
+        <p>
+          대표전화 : 1544-1900 | FAX : 0502-987-5711 | 통신판매업신고번호 :
+          제653호
+        </p>
         <p>© KOBO BOOKSTORE. All rights reserved.</p>
       </div>
     </footer>
